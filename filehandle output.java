4db857a403b9c8a0b247e6489b47da7439998735
dharani-nn <nndharani2510@gmail.com>
2025-05-22 11:19:19 +0530

    Create filehandle output.java

---
+++ filehandle output.java
@@ -0,0 +1,9 @@
+Reading file contents:
+This is the first line.
+This is the second line.
+
+After modification:
+This is the first line.
+This is the modified line.
+
+=== Code Execution Successful ===
